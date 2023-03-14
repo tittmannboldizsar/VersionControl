d5d83845cf51dadddc868f9409db03c71d183d93
mvq7ww <mvq7ww@SZA0030898>
2023-03-14 08:55:46 +0100

    CreateExcel & CreateTable commit

--- Excel_mvq7ww/Excel_mvq7ww/Form1.cs
+++ Excel_mvq7ww/Excel_mvq7ww/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System;
 
 namespace Excel_mvq7ww
 {
@@ -10,16 +12,69 @@ namespace Excel_mvq7ww
 
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
 
         private void LoadData()
         {
             Flats = context.Flat.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
         }
     }
 }

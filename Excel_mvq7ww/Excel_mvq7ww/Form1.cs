82ced0f3578658819a1bf0f56ae46a41a86399b1
mvq7ww <mvq7ww@SZA0030898>
2023-03-14 09:10:45 +0100

    GetCell commit

--- Excel_mvq7ww/Excel_mvq7ww/Form1.cs
+++ Excel_mvq7ww/Excel_mvq7ww/Form1.cs
@@ -75,6 +75,57 @@ namespace Excel_mvq7ww
             {
                 xlSheet.Cells[1, i + 1] = headers[i];
             }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+
+            int counter = 0;
+
+            foreach (Flat item in Flats)
+            {
+                values[counter, 0] = item.Code;
+                values[counter, 1] = item.Vendor;
+                values[counter, 2] = item.Side;
+                values[counter, 3] = item.District;
+
+
+                if (item.Elevator)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+
+                values[counter, 5] = item.NumberOfRooms;
+                values[counter, 6] = item.FloorArea;
+                values[counter, 7] = item.Price;
+                values[counter, 8] = "";
+
+                counter++;
+
+            }
+
+            xlSheet.get_Range(
+                        GetCell(2, 1),
+                        GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
